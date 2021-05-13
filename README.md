cf0347499b8a6c2240cdfd390d6e283b597677a1
mpindaro <margherita.pindaro@studenti.unimi.it>
2021-05-13 19:36:17 +0200

    fixed attached files download

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ Scarica tutti materiali e le videolezioni di un corso da un certo sito didattico
 
 ## Come funziona
 
-Vai sulla pagina del corso che ti interessa, e premi Ctrl + U per visualizzare il sorgente pagina. Salvalo nel file page.txt (stessa cartella) e bon fai partire lo script python o il notebook, sono la stessa cosa
+Vai sulla pagina del corso che ti interessa, e premi Ctrl + U per visualizzare il sorgente pagina. Salvalo nel file page.txt (stessa cartella) e bon fai partire lo script python o il notebook, sono la stessa cosa.
+
+Nel caso il sito da cui si sta scaricando ha materiali in allegato bisogna fornire l'url del sito e il cookie arielauth
+
+Esempio di url: https://mpindarovs."mermaid".ctu.uni"me".it/v5
+
+Capiate le virgolette
 
 ## Requirements
 
--- mermaidDownloader.py
+++ mermaidDownloader.py
@@ -11,18 +11,27 @@ def downloadFiles():
 
 	materials_nonflat  = [ div.find_all("a") for div in soup.find_all("div", class_="arielMessageBody")]
 	materials = [item["href"] for sublist in materials_nonflat for item in sublist]
+	
+
 	attacched_materials_non_flat =  [ div.find_all("a") for div in soup.find_all("div", class_="arielAttachmentBox")]
 	sitoAriel=""
+	cookies = ""
 	if len(attacched_materials_non_flat)!=0:
-    		sitoAriel = input("Inserire il link del sito ariel")
-	attached_materials = [sitoAriel + re.search( '(/.+)', item["href"]).group(1) for sublist in attacched_materials_non_flat for item in sublist]
-	materials = materials + attached_materials
+	    sitoAriel = input("Inserire il link del sito ariel")
+	    arielauth = input("Inserisci il cookie arielauth")
+	    cookies = {"arielauth": f"{arielauth}"}
+	attached_materials = [{"url":sitoAriel + re.search( '(/.+)', item["href"]).group(1), "name":item.getText() }for sublist in attacched_materials_non_flat for item in sublist]
 	
 	print('Sto scaricando slide e altri materiali')
 	for materiale in materials:
-    		r = requests.get(materiale, allow_redirects=True)
-    		m = re.search('.+/(.+)', materiale).group(1)
-    		with open('Result/' + m , 'wb+') as f:
+	    m = re.search('.+/(.+)', materiale).group(1).strip('/')
+	    r = requests.get(materiale, allow_redirects=True)
+	    with open('Result/' + m , 'wb+') as f:
+	       f.write(r.content)
+
+	for materiale in attached_materials:
+	    r = requests.post(materiale["url"], allow_redirects=True, cookies=cookies)
+	    with open('Result/' + materiale["name"] , 'wb+') as f:
 		f.write(r.content)
 	print("Ho finito di scaricare slide e altri materiali. Inizio a scaricare le videolezioni")
 

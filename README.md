f7ed367f63d8b98f02b8b8464adf57926ed1b9e6
mpindaro <margherita.pindaro@studenti.unimi.it>
2021-05-25 09:13:56 +0200

    fixed bug, added parameters to script and added requirements

--- README.md
+++ README.md
@@ -6,21 +6,36 @@ Tool che scarica tutti materiali (slide e simili) e le videolezioni di un corso 
 ![](https://thumbs-prod.si-cdn.com/YVaC3lqx4bk9jPIThb-0RJnKbbw=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/26/34/26349ee5-df4d-4595-8d77-674a8ef40fc0/t03pxm.jpg)
 ## Utilizzo
 
-Il tool chiede in ingresso due elementi:
+Il tool chiede in input due elementi:
 - Il link della **sezione** del sito  🧜Ariel🧜 d'interesse. Detto terra terra, la pagina dove sono contenuti i materiali e/o le registrazioni.
 - Il cookie **arielauth**. Per ottenerlo basta aprire da un qualsiasi sito 🧜Ariel🧜 i Developers Tools, **è necessario essere loggati**. In particolare mi riferirò a Chrome, ma dovrebbe essere circa lo stesso anche per gli altri browser. Da lì ```Application > Storage > Cookies > Cookies di uni*me*```. A questo punto dovreste trovarvi una tabella con header Name - Value - Domain - etc. Di questa tabella ci interessa, come detto prima, solo il cookie arielauth. Trovatelo, copiate il valore e datelo in pasto al tool. È importante che il cookie sia ancora caldo di forno (**dovete esservi loggati da poco**).
 
-Ho messo sia uno script Python che un Notebook Jupyter, sono la stessa cosa, usate quello che preferite. Non richiede parametri e scaricherà tutto in una cartella ```Result```, in particolare i video saranno sotto ```Result/videos```
+Ho messo sia uno script Python che un Notebook Jupyter, usate quello che preferite. Verrà scaricato tutto in una cartella ```Result```, in particolare i video saranno sotto ```Result/videos```
 
 ⚠️Se i file scaricati sono corrotti o illeggibili significa che il cookie è scaduto nel mentre e che va reinserito. Questo discorso vale per le slide, i video non ne hanno bisogno⚠️
 
-Per domande mpindaro su telegram se invece a leggere è qualcuno dell'università sono Manuel Dileo
+Per domande mpindaro su telegram se invece a leggere è qualcuno dell'università sono Manuel Dileo.
+
+### Script Python
+```python3 downloader.py [-video] [-slide] url cookie```
+
+Parametri obbligatori (spiegati sopra):
+- ```url``` 
+- ```cookie```
+
+Parametri opzionali:
+- ```-video``` per scaricare solo i video
+- ```-slide``` per scaricare solo slide e materiali
+
+Se assenti verrà scaricato tutto. 
+
+### Notebook
+Hai totale libertà evviva! Ma ricorda... [I Don't Like Notebooks](https://docs.google.com/presentation/d/1n2RlMdmv1p25Xy5thJUhkKGvjtV-dkAIsUXP-AL4ffI/edit#slide=id.g362da58057_0_1)
+
 
 ## Requirements
 
 - Python 3
-- requests: ```pip install requests```
-- os
-- Beautifoul soup: ```pip install beautifulsoup4```
+- Requests e Beautifoul Soup installabili con ```pip install -r requirements.txt```
 - ffmpeg: [Download](https://www.ffmpeg.org/download.html)
 
---
+++ downloader.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+from bs4 import BeautifulSoup as bs
+import re
+import requests
+import os
+import sys
+
+
+def downloadFiles(askedVideos, askedFiles, link, arielauth):
+	cookies = {"arielauth": f"{arielauth}"}
+
+
+	r = requests.post(link, allow_redirects=True, cookies=cookies)
+	soup = bs(r.text, "html.parser")
+
+	
+	if askedFiles:
+		materials_nonflat  = [ div.find_all("a") for div in soup.find_all("div", class_="arielMessageBody")]
+		materials = [item["href"] for sublist in materials_nonflat for item in sublist]
+
+
+
+		attacched_materials_non_flat =  [ div.find_all("a") for div in soup.find_all("div", class_="arielAttachmentBox")]
+		sitoAriel=""
+		if len(attacched_materials_non_flat)!=0:
+			sitoAriel = re.search("(https://[\w]+.ariel.ctu.unimi.it/[\w]+)", link).group(1)
+		attached_materials = [{"url":sitoAriel + re.search( '(/.+)', item["href"]).group(1), "name":item.getText() }for sublist in attacched_materials_non_flat for item in sublist]
+
+
+		for materiale in materials:
+			print(f"Sto scaricando {materiale}")
+			m = re.search('.+/(.+)', materiale).group(1).strip('/')
+			r = requests.get(materiale, allow_redirects=True)
+			os.makedirs(os.path.dirname("Result/"), exist_ok=True)
+			with open('Result/' + m , 'wb+') as f:
+				f.write(r.content)
+
+
+		for materiale in attached_materials:
+			print(f"Sto scaricando {materiale['name']}")
+			r = requests.post(materiale["url"], allow_redirects=True, cookies=cookies)
+			os.makedirs(os.path.dirname("Result/"), exist_ok=True)
+			with open('Result/' + materiale["name"] , 'wb+') as f:
+				f.write(r.content)
+		print("Ho finito di scaricare slide e altri materiali.")
+	
+	if askedVideos:
+
+		videos = [ div.find("video").find("source")["src"] for div in soup.find_all("div", class_="embed-responsive embed-responsive-16by9")]
+
+		i=1
+		for video in videos:
+			m = re.search('.+/mp4:(.+)(.mp4|.MP4)/.+', video).group(1).replace("%20", " ")
+			print(f'Sto scaricando {m}. Progresso: {i}/{len(videos)}')
+			os.makedirs(os.path.dirname("Result/videos/"), exist_ok=True)
+			command = f'ffmpeg -i "{video}" -loglevel quiet -y -c copy "Result/videos/{m}.mp4"'
+			if os.system(command):
+				raise RuntimeError(f'program {command} failed!')
+			print(f'Ho finito di scaricare {m}')
+			i=i+1
+	print("Finito!")
+
+def readInputs():
+	inputs = sys.argv[1:]
+	if len(inputs) == 0 or len(inputs)==1:
+		print("Mancano parametri. Mi servono almeno il sito ariel e il cookie!")
+		return
+	if len(inputs) == 2:
+		if bool(re.search('(https://\w+.ariel.ctu.unimi.it/.+)',inputs[0])):
+			downloadFiles(True, True, inputs[0], inputs[1])
+		else:
+			print("Link non valido")
+			return
+	else:
+		if bool(re.search('(https://\w+.ariel.ctu.unimi.it/.+)',inputs[-2])):
+			downloadFiles("-video" in inputs, "-slide" in inputs, inputs[-2], inputs[-1])
+		else:
+			print("Link non valido")
+			return
+
+
+if __name__=="__main__":
+	readInputs()
+
